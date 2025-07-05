e9a98ff6963b350531fb921d3b19b0e124d159f1
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-07-05 19:06:25 +0400

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+// App.jsx
+import NavBar from "./navBar";
+import Home from "./home";
+import About from "./about";
+import Program from "./project";
+import Footer from "./footer";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <About />
+      <Program />
+      <Footer />
+    </>
+  );
+}
