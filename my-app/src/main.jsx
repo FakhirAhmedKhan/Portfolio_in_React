27b2422a5e89a4ee11ee2f36d69163d1d0fec5be
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-07-05 18:01:30 +0400

    fixing

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,24 +1,7 @@
 import "./input.css";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import NavBar from "./navBar";
-import Home from "./home";
-import About from "./about";
-import Program from "./project";
-import Footer from "./footer";
-
-// Optionally, you can wrap with a Fragment for clarity
-function App() {
-  return (
-    <>
-      <NavBar />
-      <Home />
-      <About />
-      <Program />
-      <Footer />
-    </>
-  );
-}
+import App from "./app";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
