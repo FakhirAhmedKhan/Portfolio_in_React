83440b77dfd66dd638d9d8f67f5491da0069d642
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-07-06 12:12:16 +0400

    remove duplicate file

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-import tailwindcss from "tailwindcss"; // Correct import: import 'tailwindcss'
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [
-    tailwindcss(), // Correct usage: call it as a function
-    autoprefixer,
-  ],
-};
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/Portfolio_in_React/", // <== VERY IMPORTANT
-  plugins: [react()],
-});
